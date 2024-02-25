2bd1f144646a68cc8131546d0142e979f35a476c
Diana PF <149829535+KitsuneDai@users.noreply.github.com>
2024-02-25 09:37:24 +0000

    proyecto casi terminado

--- src/js/component/Tasklist.jsx
+++ src/js/component/Tasklist.jsx
@@ -5,35 +5,45 @@ const Tasklist = () => {
   const [todos, setTodos] = useState([]);
   const [task, setTask] = useState("");
 
-
   const urlTodos =
     "https://playground.4geeks.com/apis/fake/todos/user/KitsuneDai";
 
-    useEffect(() => {
-      fetchTasks();
-    }, []);
-  
-    const fetchTasks = () => {
-      fetch(urlTodos)
-        .then((response) => response.json())
-        .then((data) => {
-          setTodos(data);
-          console.log("Tareas obtenidas:", data);
-        })
-        .catch((err) => {
-          console.error("Error al obtener las tareas:", err);
-        });
-    };
-
  
-  const addTask = () => {
-    let newTodo = { label: task, done: false };
+
+  //POST --> Para crear un usuario
+  useEffect(() => {
     fetch(urlTodos, {
-      method: "PUT", 
+      method: "POST",
+      body: JSON.stringify([]),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log("POST->", data);
+      })
+      .catch((err) => {
+        return err;
+      });
+  }, []);
+
+  //PUT --> para añadir las tareas
+  const  addTask =()=> {
+    if (!task.trim()) {
+      //Para que no me agregue una tarea vacía
+      alert("Por favor, ingresa una tarea.");
+      return;
+    }
+    const newTodo = {label: task, done: false };
+    fetch(urlTodos, {
+      method: "PUT",
       body: JSON.stringify([...todos, newTodo]), // Agrega la nueva tarea al array existente
       headers: { "Content-Type": "application/json" },
     })
-      .then((response) => response.json())
+      .then((response) => {
+        return response.json();
+      })
       .then((data) => {
         setTodos([...todos, newTodo]);
         setTask(""); // Limpia el campo de entrada después de agregar la tarea
@@ -42,83 +52,130 @@ const Tasklist = () => {
       .catch((err) => {
         console.error(err);
       });
-  };
-
-
-  const deleteTask = (taskId) => {
-    fetch( urlTodos , {
-      method: "DELETE",
-    })
-      .then(() => {
-        setTodos(todos.filter((todo) => todo.id !== taskId));
-        console.log("Eliminar:", taskId);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
-
-  const buttonYes =() => {
-    fetch("https://playground.4geeks.com/apis/fake/todos/user/KitsuneDai", {
-      method: "DELETE",
-    })
-    .then (()=>{
-      setTodos([]);
-      console.log("Todas las tareas eliminadas");
-    })
-    .catch((err) => {
-      console.err(err);
-    })
   }
 
-  const buttonNo =()=>{
+   //GET --> Me trae la lista que he creado
+   useEffect(() => {
+    fetch(urlTodos)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setTodos(data);
+        console.log("Tareas obtenidas:", data);
+      })
+      .catch((err) => {
+        return err, console.error("Error al obtener las tareas:", err);
+      });
+  }, [todos.length]);
+
+  //DELETES
+  //para eliminar una tarea
+  const deleteTask = (taskId) => {
+    const updatedTodos = todos.filter(todo => todo.id !== taskId); // Eliminar la tarea del estado de todos
+    fetch(urlTodos, {
+      method: "PUT",
+      body: JSON.stringify(updatedTodos),
+      headers: { "Content-Type": "application/json" },
+    })
+    .then((response) => {
+      if (response.ok) {
+        setTodos(updatedTodos); // Actualizar el estado solo si la actualización del backend es exitosa
+        console.log("PUT-> Tarea eliminada exitosamente");
+      } else {
+        console.error("Error al eliminar la tarea");
+      }
+    })
+  .catch((err) => {
+    console.error("Error al eliminar la tarea:", err);
+  });
+  };
+
+  //para eliminar toda la lista
+  const buttonYes = () => {
+    const clearnList = todos.length > 0 ? todos : [{ label: "Sonríe :)", done: false }];
+    fetch(urlTodos, {
+      method: "PUT",
+      body: JSON.stringify(clearnList),
+      headers: { "Content-Type": "application/json" },
+    })
+    .then((response) => {
+      if (response.ok) {
+        setTodos([]); // Actualizar el estado con un arreglo vacío
+        console.log("PUT-> Todas las tareas han sido eliminadas exitosamente");
+      } else {
+        console.error("Error al eliminar todas las tareas");
+      }
+    })
+    .catch((err) => {
+      console.error("Error al eliminar todas las tareas:", err);
+    });
+
+    
+  };
+
+  const buttonNo = () => {
     alert("¡Pues deja de tocar cosas!");
   };
 
- 
   return (
     <>
       <ul>
-        <li> 
-        <input type="text" 
-             onChange={(e)=>setTask(e.target.value)} //Traemos un valor que nos proporciona el input al setTask
-             value={task} // Añadimos el valor que entró desde el input a la variable llamada task
-             onKeyDown={(e)=> {
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setTask(e.target.value)} //Traemos un valor que nos proporciona el input al setTask
+            value={task} // Añadimos el valor que entró desde el input a la variable llamada task
+            onKeyDown={(e) => {
               if (e.key === "Enter") {
                 addTask();
-                
               }
-          }} 
-             className="Addtask"
-             placeholder="What needs to be done?">
-            </input>
+            }}
+            className="Addtask"
+            placeholder="What needs to be done?"
+          ></input>
         </li>
 
         {todos.map((todo) => (
           //Añado un key que se genere para cada child de forma única.
-          <li key={`task-${todo.label}`} className="tasklist">  
+          <li key={`task-${todo.label}`} className="tasklist">
             {todo.label}
-            <span onClick={() => deleteTask(todo.id)} className="tasklist"><RiDeleteBinLine className="icon" /></span>
+            <span onClick={() => deleteTask(todo.id)} className="tasklist">
+              <RiDeleteBinLine className="icon" />
+            </span>
           </li>
         ))}
-        
       </ul>
 
       <hr />
 
       <div className="footer">
-        {todos.length === 0 ? (<p>No hay tareas en la lista, añada una tarea</p>) : todos.length === 1 ? (`${todos.length} tarea`) : (`${todos.length} tareas`)}
+        {todos.length === 0 ? (
+          <p>No hay tareas en la lista, añada una tarea</p>
+        ) : todos.length === 1 ? (
+          `${todos.length} tarea`
+        ) : (
+          `${todos.length} tareas`
+        )}
       </div>
+      <p className="text-center" style={{ marginBottom: "0px" }}>
+          ¿Quieres mandarlo todo a la mierda?
+      </p>
 
-      <div className="d-grid gap-2" style={{paddingBottom: "15px"}}>
-          <p className="text-center" style={{marginBottom: "0px"}}>¿Quieres mandarlo todo a la mierda?</p>
-              <button onClick={buttonYes} className="btn btn-primary" type="button">Sí quiero</button>
-              <button onClick={buttonNo} className="btn btn-danger buttonNo" type="button">No quiero</button>
+      <div className="d-flex gap-2 justify-content-center" style={{ paddingBottom: "15px" }}>
+        <button onClick={buttonYes} className="btn btn-primary" type="button">
+          Sí quiero
+        </button>
+        <button
+          onClick={buttonNo}
+          className="btn btn-danger buttonNo"
+          type="button"
+        >
+          No quiero
+        </button>
       </div>
-
     </>
   );
 };
 
 export default Tasklist;
-
